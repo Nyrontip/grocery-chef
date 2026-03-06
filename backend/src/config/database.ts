c31ca3c6b6da85b427fd5a153c31d75addd7bc57
Nyrontip <simondavidcruzsuazo@gmail.com>
2026-03-06 11:55:59 -0500

    fix: Arreglo pequeño conflicto de variable de entorno

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,18 +1,20 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
 dotenv.config();
-
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_HOST:", process.env.DB_HOST);
 const sequelize = new Sequelize(
-    process.env.DB_NAME as string,
-    process.env.DB_USER as string,
-    process.env.DB_PASSWORD as string,
-    {
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT) || 5433,
-        dialect: 'postgres',
-        logging: false,
-    }
+  process.env.DB_NAME as string,
+  process.env.DB_USER as string,
+  process.env.DB_PASSWORD as string,
+  {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 5432,
+    dialect: "postgres",
+    logging: false,
+  },
 );
 
 export default sequelize;
+
