0b1f3b1d08740062880f59473167161cb0ede2b8
Nyrontip <simondavidcruzsuazo@gmail.com>
2026-03-06 22:03:53 -0500

    fix: delete next files

--- frontend/.next/dev/static/chunks/src_app_login_page_tsx_8e9f1618._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_styles_login_3049af14.css"
-],
-    source: "dynamic"
-});
